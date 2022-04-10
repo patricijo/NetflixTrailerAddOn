18038b4438b42b2c71f15a9869ed86cad2611bb1
PatriciJo <sebastian.patrici@gmail.com>
2022-04-10 21:41:23 +0200

    update

--- script.js
+++ script.js
@@ -1,17 +1,100 @@
-$(".about-header").each(function () {
-  const title = encodeURIComponent($(this).find("strong").text());
+const icon =
+  '<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512" style="margin: -6px;height: 30px;"><rect x="48" y="96" width="416" height="320" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect><rect x="384" y="336" width="80" height="80" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect><rect x="384" y="256" width="80" height="80" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect><rect x="384" y="176" width="80" height="80" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect><rect x="384" y="96" width="80" height="80" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect><rect x="48" y="336" width="80" height="80" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect><rect x="48" y="256" width="80" height="80" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect><rect x="48" y="176" width="80" height="80" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect><rect x="48" y="96" width="80" height="80" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect><rect x="128" y="96" width="256" height="160" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect><rect x="128" y="256" width="256" height="160" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect></svg>';
 
-  const YTurl =
-    "https://www.youtube.com/results?search_query=" + title + "%20trailer";
+const tooltip = '<div class="NF-trailer-tooltip">Trailer</div>';
 
-  const icon =
-    '<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512" style="margin: -6px;;height: 30px;"><rect x="48" y="96" width="416" height="320" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect><rect x="384" y="336" width="80" height="80" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect><rect x="384" y="256" width="80" height="80" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect><rect x="384" y="176" width="80" height="80" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect><rect x="384" y="96" width="80" height="80" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect><rect x="48" y="336" width="80" height="80" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect><rect x="48" y="256" width="80" height="80" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect><rect x="48" y="176" width="80" height="80" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect><rect x="48" y="96" width="80" height="80" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect><rect x="128" y="96" width="256" height="160" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect><rect x="128" y="256" width="256" height="160" rx="28" ry="28" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"></rect></svg>';
+////////////////////////
+//// OBSERVER
+////////////////////////
 
-  const tooltip = '<div class="NF-trailer-tooltip">Trailer</div>';
+var observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutation) {
+    if (mutation.addedNodes.length) {
+      if (
+        mutation.addedNodes[0]
+          .querySelector("div")
+          .classList.contains("mini-modal")
+      ) {
+        addButtonToPreview();
+      }
 
-  const trailerBtn = `<a href="${YTurl}" target="_blank"><button class="trailer-button color-supplementary round ltr-1knzl35">${
-    icon + tooltip
-  }</button></a>`;
-
-  $(".buttonControls--container").append(trailerBtn);
+      if (
+        mutation.addedNodes[0]
+          .querySelector("div")
+          .classList.contains("detail-modal")
+      ) {
+        addButtonToDetail();
+      }
+    }
+  });
 });
+
+var wrapper = document.querySelector('[dir="ltr"]').querySelector("div");
+observer.observe(wrapper, {
+  childList: true,
+});
+
+////////////////////////
+//// ADD TO PREVIEW
+////////////////////////
+
+const addButtonToPreview = () => {
+  var item = document.querySelector(".focus-trap-wrapper");
+
+  var title = item
+    .querySelector("img.previewModal--boxart")
+    .getAttribute("alt");
+
+  var url =
+    "https://www.youtube.com/results?search_query=" +
+    encodeURIComponent(title) +
+    "%20trailer";
+
+  var trailerBtn = `<button onClick="window.open('${url}', '_blank')" class="trailer-button color-supplementary round ltr-1knzl35">${
+    icon + tooltip
+  }</button>`;
+
+  item
+    .querySelector(".primary-button")
+    .insertAdjacentHTML("afterend", trailerBtn);
+
+  //adding the button when the modal-mini transforms to modal-detail
+  item.querySelector(".mini-modal-container").onclick = function () {
+    setTimeout(function () {
+      addButtonToDetail();
+    }, 500);
+  };
+};
+
+////////////////////////
+//// ADD TO DETAIL
+////////////////////////
+
+const addButtonToDetail = () => {
+  if (document.querySelector(".about-header")) {
+    var item = document.querySelector(".focus-trap-wrapper");
+
+    var title = item
+      .querySelector(".about-header")
+      .querySelector("strong").innerHTML;
+
+    var url =
+      "https://www.youtube.com/results?search_query=" +
+      encodeURIComponent(title) +
+      "%20trailer";
+
+    var trailerBtn = `<button onClick="window.open('${url}', '_blank')" class="trailer-button color-supplementary round ltr-1knzl35">${
+      icon + tooltip
+    }</button>`;
+
+    item
+      .querySelector(".primary-button")
+      .insertAdjacentHTML("afterend", trailerBtn);
+  }
+};
+
+// add button on page load
+
+if (document.querySelector(".focus-trap-wrapper")) {
+  addButtonToDetail(document.querySelector(".focus-trap-wrapper"));
+}
